e6a82edf3d3dd3035fd8cf81a438cfe8f694c9a9
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2025-04-11 17:18:44 +1000

    feat: checkpoint

--- app/models/user.ts
+++ app/models/user.ts
@@ -27,4 +27,7 @@ export default class User extends compose(BaseModel, AuthFinder) {
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   declare updatedAt: DateTime | null
+
+  @column()
+  declare hasPublicProfile: boolean
 }
