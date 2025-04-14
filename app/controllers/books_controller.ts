451ff0611de6981e6a7c4c79ca9d36e1ea48bc29
Justin Taylor <26264269+sjustintaylor@users.noreply.github.com>
2025-04-15 08:59:20 +1000

    feat: checkpoint

--- app/controllers/books_controller.ts
+++ app/controllers/books_controller.ts
@@ -3,13 +3,14 @@ import type { HttpContext } from '@adonisjs/core/http'
 export default class BooksController {
   /**
    * Display book search page
+   * Also returns search results if request contains query params for it
    */
   async index({ request, view }: HttpContext) {
     return view.render('pages/books/search')
   }
 
   /**
-   * Handle form submission for the create action
+   * Handle form submission for the create book action (if the book is being added manually)
    */
   async store({ request }: HttpContext) {}
 }
